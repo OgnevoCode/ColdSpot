ba53d1f37aafd7307989ee25f687fb66f4b7098f
OgnevoCode <126795952+OgnevoCode@users.noreply.github.com>
2023-06-12 10:12:08 +0300

    Update Main.java

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -13,10 +13,10 @@ import com.mygdx.game.Screens.GameOverScreen;
 import com.mygdx.game.Screens.GameScreen;
 import com.mygdx.game.Screens.SplashScreen;
 import com.mygdx.game.Screens.WinScreen;
-//Main class loads some game objects and controls screen changes
+//MAIN CLASS LOADS SOME GAME OBJECTS AND RENDERS SCREENS
 public class Main extends Game {
 	public static SpriteBatch batch;
-	public static  ScreenViewport viewport = new ScreenViewport();
+	public static ScreenViewport viewport = new ScreenViewport();
 	public static OrthographicCamera camera;
 	public static final float PPM = 64;
 	public static final short BIT_WALL = 2;
@@ -28,40 +28,10 @@ public class Main extends Game {
 	public static ShapeRenderer shapeRenderer;
 
 	private SplashScreen splashScreen;
-	private GameScreen gameScreen;
-	private GameOverScreen gameOverScreen;
-	private WinScreen winScreen;
-	public final static int SPLASH = 0;
-	public final static int GAME = 1;
-	public final static int GAMEOVER = 2;
-	public final static int WIN = 3;
 
 	@Override
 	public void render() {
 		super.render();
-	}
-
-	public void changeScreen(int screen){
-		try {
-			switch (screen) {
-				case SPLASH:
-					if (splashScreen == null) splashScreen = new SplashScreen(this);
-					this.setScreen(splashScreen);
-					break;
-				case GAME:
-					if (gameScreen == null) gameScreen = new GameScreen(this);
-					this.setScreen(new GameScreen(this));
-					break;
-				case GAMEOVER:
-					if (gameOverScreen == null) gameOverScreen = new GameOverScreen(this);
-					this.setScreen(gameOverScreen);
-					break;
-				case WIN:
-					if (winScreen == null) winScreen = new WinScreen(this);
-					this.setScreen(winScreen);
-					break;
-			}
-		} catch (NullPointerException e) {}
 	}
 
 	@Override
@@ -87,6 +57,5 @@ public class Main extends Game {
 		batch.dispose();
 		shapeRenderer.dispose();
 		splashScreen.dispose();
-		gameScreen.dispose();
 	}
 }
